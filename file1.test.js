cbe85bfceb8678e914c38268fe0b0be5e3f1dd29
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-23 22:54:04 +0900

    remove some test, add parallel test files

--- file1.test.js
+++ file1.test.js
@@ -13,14 +13,6 @@ describe('file1:describe1', () => {
     appendFileSync(global.logPath, 'file1:end of test1-1\n');
   });
 
-  it('file1:test1-2', () => {
-    appendFileSync(global.logPath, 'file1:beginning of test1-2\n');
-
-    expect(1-2).toBe(1-2);
-
-    appendFileSync(global.logPath, 'file1:end of test1-2\n');
-  });
-
   describe('file1:describe1-1', () => {
     appendFileSync(global.logPath, 'file1:beginning of describe1-1\n');
 
@@ -58,6 +50,30 @@ describe('file1:describe2', () => {
   });
 
   appendFileSync(global.logPath, 'file1:end of describe2\n');
+});
+
+it('file1:test3', () => {
+  appendFileSync(global.logPath, 'file1:beginning of test3\n');
+
+  expect(3).toBe(3);
+
+  appendFileSync(global.logPath, 'file1:end of test3\n');
+});
+
+beforeAll(() => {
+  appendFileSync(global.logPath, 'file1:beforeAll at file scope\n');
+});
+
+beforeEach(() => {
+  appendFileSync(global.logPath, 'file1:beforeEach at file scope\n');
+});
+
+afterAll(() => {
+  appendFileSync(global.logPath, 'file1:afterAll at file scope\n');
+});
+
+afterEach(() => {
+  appendFileSync(global.logPath, 'file1:afterEach at file scope\n');
 });
 
 appendFileSync(global.logPath, 'file1:end of file\n');
