0bd04c47600c3966ef4658f7278ae0b7f1afdb81
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-24 18:14:50 +0900

    add sequential tests

---
+++ sequential/file1.test.js
@@ -0,0 +1,22 @@
+const basename = require('path').basename(global.basename, '.test.js');
+const debug = require('../src/logger')(`jest:sequential:${basename}`);
+
+const wait = require('../src/wait');
+
+debug('beginning of file');
+
+beforeAll(() => { debug('beforeAll'); });
+beforeEach(() => { debug('beforeEach'); });
+afterAll(() => { debug('afterAll'); });
+afterEach(() => { debug('afterEach'); });
+
+[1, 1800, 700, 1, 300, 1].map((delay, i) => {
+  const no = i + 1;
+  it(`test${no}`, async () => {
+    debug('test%s (start)', no);
+    expect(await wait(delay)).toBe(true);
+    debug('test%s (end)', no);
+  });
+});
+
+debug('end of file');
